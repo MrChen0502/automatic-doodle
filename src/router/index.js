a78c0ad9025cf730d48344010ec56155010fc15d
霓壹 <c3568678712@outlook.com>
2026-05-26 17:46:30 +0800

    修改登录

--- src/router/index.js
+++ src/router/index.js
@@ -11,7 +11,7 @@ const routes = [
     component: () => import('@/App.vue')
   },
   {
-    path: '/login1',
+    path: '/logines',
     name: 'LoginCom',
     component: () => import('@/views/LoginCom.vue')
   },
