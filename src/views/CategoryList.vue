56e2e61df6d14fa442458fc3ef7dfa59823a0225
霓壹 <chenganghao9@gmail.com>
2026-06-08 18:31:37 +0800

    提交

--- src/views/CategoryList.vue
+++ src/views/CategoryList.vue
@@ -1,7 +1,13 @@
 <template>
     <el-card>
-        <el-button type="primary"  plain>添加分类</el-button>
+        <el-button type="primary" plain>添加分类</el-button>
     </el-card>
+
+    <el-table stripe>
+        <el-table-column>
+            
+        </el-table-column>
+    </el-table>
 </template>
 
 <script>
@@ -9,6 +15,9 @@
 </script>
 
 <style scoped>
-
+.el-card{
+    height: calc(98vh - 145px);
+    margin-top: 15px;
+}
 
 </style>
